a5a67aa118edf44ab3b07e6d661c9e58cd24945c
alberttgomes <albertgomes1995@hotmail.com>
2022-03-27 16:15:53 -0300

    my app using flat list, discipline programming to mobile device

--- App.js
+++ App.js
@@ -14,7 +14,11 @@ const picture2 = require("./assets/boy.jpg");
 const picture3 = require("./assets/thiago.jpg");
 const picture4 = require("./assets/korra-children-lin.jpg");
 const picture5 = require("./assets/korra.jpeg");        
-
+const picture6 = require("./assets/iroh.jpg");
+const picture7 = require("./assets/aang.jpg");
+const picture8 = require("./assets/toffee.jpg");
+const picture9 = require("./assets/tessei.jpg");
+const picture10 = require("./assets/ozai.jpg");
 
 const DATA = [
   {
@@ -33,27 +37,62 @@ const DATA = [
   },
   {
     id: '2',
-    firstName: 'Guilherme',
-    lastName: 'Zaicaner',
-    age: '56',
+    firstName: 'Emerson',
+    lastName: 'Lucas',
+    age: '20',
     avatar: picture2
   },
   {
     id: '3',
+    firstName: 'Guilherme',
+    lastName: 'Zaicaner',
+    age: '56',
+    avatar: picture6
+  },
+  {
+    id: '4',
     firstName: 'Thiago',
     lastName: 'Guedes',
     age: '22',
     avatar: picture3
   },
   {
-    id: '4',
-    firstName: 'Wdyla',
-    lastName: 'Carvalho',
-    age: '19',
+    id: '5',
+    firstName: 'Thiago',
+    lastName: 'Arruda',
+    age: '24',
+    avatar: picture7
+  },
+  {
+    id: '6',
+    firstName: 'Rita',
+    lastName: 'Cássia',
+    age: '27',
     avatar: picture4
   },
   {
-    id: '5',
+    id: '7',
+    firstName: 'Rodrigo',
+    lastName: 'Fernandes',
+    age: '22',
+    avatar: picture10
+  },
+  {
+    id: '8',
+    firstName: 'Vinicius',
+    lastName: 'Maia',
+    age: '25',
+    avatar: picture9
+  },
+  {
+    id: '9',
+    firstName: 'Wdyla',
+    lastName: 'Carvalho',
+    age: '19',
+    avatar: picture8
+  },
+  {
+    id: '10',
     firstName: 'Yohana',
     lastName: 'Texeira',
     age: '20',
@@ -89,7 +128,6 @@ const App = () => {
       <View style={styles.view}>
         <Button
             title='PRESS ME'
-            style={styles.button}
             onPress={() => alert("This is an list of students that course time")}
         />
       </View>
@@ -120,7 +158,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
   },
   title: {
-    fontSize: 25,
+    fontSize: 23,
     fontWeight: "bold",
     color: '#FFFFFF'
   },
@@ -132,11 +170,6 @@ const styles = StyleSheet.create({
   },
   view:{
     margin: 20
-  },
-  button: {
-    fontWeight: 'bold',
-    height: 150,
-    width: 180
   }
 });
 
